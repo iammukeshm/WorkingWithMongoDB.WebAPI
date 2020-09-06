540babe671164567f40ec5d84633e75adf7bbc4d
Mukesh Murugan <iammukeshm@gmail.com>
2020-09-06 14:44:44 +0530

    added customer model

---
+++ WorkingWithMongoDB.WebAPI/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithMongoDB.WebAPI.Models
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [BsonElement("PhoneNumber")]
+        public string Contact { get; set; }
+        public string Email { get; set; }
+    }
+}
